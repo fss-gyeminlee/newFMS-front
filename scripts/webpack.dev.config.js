615feb500ad5f6a90404238094e006b49de5fb79
gyemin <gyemin.lee@fssuniverse.com>
2022-07-13 13:33:59 +0900

    - port 8888 -> 8887 변경

--- scripts/webpack.dev.config.js
+++ scripts/webpack.dev.config.js
@@ -14,7 +14,7 @@ try {
   selfIp = 'localhost'
 }
 
-const PORT = 8888
+const PORT = 8887
 function getIpAddress () {
   const interfaces = require('os').networkInterfaces
   for (let devName in interfaces) {

ab268ab8a1b6c891da3a230b0840bebac7f35320
gyemin <gyemin.lee@fssuniverse.com>
2022-07-20 17:17:22 +0900

    - react query 샘플 적용

--- app/client.js
+++ app/client.js
@@ -1,17 +1,23 @@
 // import '@babel/polyfill'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { hot } from 'react-hot-loader/root'
-import '@config'
-import Routes from '@configs/router.config'
-import configure from '@middleware/configureStore'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { hot } from 'react-hot-loader/root';
+import '@config';
+import Routes from '@configs/router.config';
+import configure from '@middleware/configureStore';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
-const HotRoutes = hot(Routes)
-const store = configure({ })
+const queryClient = new QueryClient();
+
+
+const HotRoutes = hot(Routes);
+const store = configure({});
 ReactDOM.render(
-  <Provider store={store}>
-    <HotRoutes />
-  </Provider>,
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <HotRoutes />
+    </Provider>
+  </QueryClientProvider>,
   document.getElementById('root'),
-)
+);
--- app/pages/menu/menu2.js
+++ app/pages/menu/menu2.js
@@ -1,5 +1,33 @@
-import React, { Component } from 'react'
+import React, { useEffect, Component } from 'react'
+import axios from 'axios'
+import { useQuery } from 'react-query'
 
-const menu1 = () => <div>menu2 area </div>
+const menu1 = () =>
 
+  /* FIXME: react query 사용 */
+  // const { data, isLoading, isFetching, isError, error } = useQuery('get-test', axios.post('http://localhost:1111/mock/usercenter/user/userInfo'),
+  //   {
+  //     onSuccess: data => {
+  //       console.log(data);
+  //     },
+  //     onError: e => {
+  //       console.log(e.message);
+  //       debugger;
+  //     }
+  //   });
+
+
+  /* FIXME: axios 사용  */
+  // useEffect(() => {
+  // axios.post('http://localhost:1111/mock/usercenter/user/userInfo')
+  //   .then(function (response) {
+  //     // handle success
+  //     debugger;
+  //     console.log(response);
+  //   })
+  // },[])
+  <div>menu2 area </div>
+
+
+// http://localhost:1111/mock/usercenter/login
 export default menu1;
